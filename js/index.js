5bae56f2c247739f19ccb7b63aee70b4c3a92219
Esmot-ara-lajuk <esmotaralajuk278@gmail.com>
2024-09-24 22:25:22 +0600

    change file Path

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /* Added blog */
 const blogBtn = document.getElementById('blog-button').addEventListener('click', function(){
-    window.location.href = "/blog.html"
+    window.location.href = "/assignment-5/blog.html"
 })
 
