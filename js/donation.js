8cc44325b912d4fa6ac08ed2f088fd98647cb227
Esmot-ara-lajuk <esmotaralajuk278@gmail.com>
2024-09-24 10:37:13 +0600

    added other donation function

--- js/donation.js
+++ js/donation.js
@@ -29,6 +29,62 @@ document.getElementById('donation-btn').addEventListener('click', function(event
         
     }
     else(
-        alert('Please Give A Valid Number. ')
+        alert('Please Give A Valid Number.')
     )   
+})
+
+/* Feni Donation */
+document.getElementById('feni-donation-button').addEventListener('click',function(event){
+    event.preventDefault();
+    
+    const feniDonationInput = document.getElementById('feni-donate-input').value;
+
+    if(!isNaN(feniDonationInput)){
+        const donateFeni = document.getElementById('donate-feni').innerText;
+        const totalMoney = document.getElementById('total-money').innerText;
+        
+        const newFeniDonateInput = parseFloat(feniDonationInput);
+        const newDonateFeni = parseFloat(donateFeni);
+        const remainingBalance = parseFloat(totalMoney);
+
+        const newFeniDonation = newFeniDonateInput +newDonateFeni;
+        const newRemainingBalance = remainingBalance - newFeniDonateInput;
+
+        document.getElementById('donate-feni').innerText = newFeniDonation;
+        document.getElementById('total-money').innerText = newRemainingBalance;
+
+        alert('Congratulation, You Have successfully Donated.')
+    }
+
+    else{
+        alert('Please Give A Valid Number.')
+    }
+})
+
+/* QUOTA DONATION */
+
+document.getElementById('quota-donation-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    
+    const quotaDonationInput = document.getElementById('quota-donation-input').value;
+
+    if(!isNaN(quotaDonationInput)){
+        const donateQuota =document.getElementById('donate-quota').innerText;
+        const totalMoney =document.getElementById('total-money').innerText;
+
+        const newQuotaDonateInput = parseFloat(quotaDonationInput);
+        const newDonateQuota = parseFloat(donateQuota);
+        const remainingBalance = parseFloat(totalMoney);
+
+        const newQuotaDonation = newQuotaDonateInput + newDonateQuota;
+        const newRemainingBalance = remainingBalance - newQuotaDonateInput;
+
+        document.getElementById('donate-quota').innerText = newQuotaDonation;
+        document.getElementById('total-money').innerText = newRemainingBalance;
+
+        alert('Congratulation, You Have successfully Donated.')
+    }
+    else{
+        alert('Please Give A Valid Number.')
+    }
 })
