56da5d9103e66cf22634309543cf3811cfa915b5
Esmot-ara-lajuk <esmotaralajuk278@gmail.com>
2024-09-24 22:23:15 +0600

    added modal

--- js/donation.js
+++ js/donation.js
@@ -4,7 +4,7 @@ document.getElementById('donation-btn').addEventListener('click', function(event
     const donateInput = document.getElementById('donate-input').value;
     
 
-    if (!isNaN(donateInput)){
+    if (!isNaN(donateInput )){
         const donateMoney = document.getElementById('donate-money').innerText;
 
         const totalMoney = document.getElementById('total-money').innerText;
@@ -22,11 +22,15 @@ document.getElementById('donation-btn').addEventListener('click', function(event
         document.getElementById('donate-money').innerText = newDonation;
         document.getElementById('total-money').innerText =newRemainingBalance;
 
-        const p =document.createElement('p');
-        p.innerText = `${newDonateInput} Taka is donated for ${title}`;
-        document.getElementById('history').appendChild(p);
+        my_modal_1.showModal();
 
-        alert('Congratulation, You Have successfully Donated.')
+        /* History */
+        const li =document.createElement('li');
+        li.className = (`p-5 rounded-lg m-5 shadow-xl bg-lime-100`);
+        li.innerText = `BDT: ${newDonateInput} taka Donated For Noakhali (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`;
+        const history = document.getElementById('history');
+        history.insertBefore(li, history.firstChild);
+        
         
     }
     else(
@@ -54,7 +58,14 @@ document.getElementById('feni-donation-button').addEventListener('click',functio
         document.getElementById('donate-feni').innerText = newFeniDonation;
         document.getElementById('total-money').innerText = newRemainingBalance;
 
-        alert('Congratulation, You Have successfully Donated.')
+        my_modal_1.showModal();
+
+        /* History */
+        const li =document.createElement('li');
+        li.className = (`p-5 rounded-lg m-5 shadow-xl bg-lime-100`);
+        li.innerText = `BDT: ${newFeniDonateInput} taka Donated For Feni (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`;
+        const history = document.getElementById('history');
+        history.insertBefore(li, history.firstChild);
     }
 
     else{
@@ -83,7 +94,14 @@ document.getElementById('quota-donation-btn').addEventListener('click',function(
         document.getElementById('donate-quota').innerText = newQuotaDonation;
         document.getElementById('total-money').innerText = newRemainingBalance;
 
-        alert('Congratulation, You Have successfully Donated.')
+        my_modal_1.showModal();
+
+        /* History */
+        const li =document.createElement('li');
+        li.className = (`p-5 rounded-lg m-5 shadow-xl bg-lime-100`);
+        li.innerText = `BDT: ${newQuotaDonateInput} taka Donated For Quota (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`;
+        const history = document.getElementById('history');
+        history.insertBefore(li, history.firstChild);
     }
     else{
         alert('Please Give A Valid Number.')
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 /* Added blog */
 const blogBtn = document.getElementById('blog-button').addEventListener('click', function(){
-    window.location.href = "/assignment-5/blog.html"
+    window.location.href = "/blog.html"
 })
+
